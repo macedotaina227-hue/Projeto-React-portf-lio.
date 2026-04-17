d2406f2f6b4c3ecf82a4446890af3415a3577874
macedotaina227-hue <macedotaina227@gmail.com>
2026-04-17 19:03:43 -0300

    Create About.jsx

---
+++ src/About.jsx
@@ -0,0 +1,13 @@
+function About() {
+  return (
+    <section>
+      <h1>Tainá Macedo Pessoa</h1>
+      <p>
+        Acadêmica em Tecnologia em Sistemas para Internet, com interesse em
+        desenvolvimento web.
+      </p>
+    </section>
+  );
+}
+
+export default About;
