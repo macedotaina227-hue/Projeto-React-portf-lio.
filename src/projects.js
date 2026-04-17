e469ec4397f2f1e41fa2571e47ba72713e30c2d0
macedotaina227-hue <macedotaina227@gmail.com>
2026-04-17 19:06:20 -0300

    Create projects.js

---
+++ src/projects.js
@@ -0,0 +1,14 @@
+const projects = [
+  {
+    id: 1,
+    nome: "Calculadora Simples",
+    descricao: "Uma calculadora feita com JavaScript que realiza operações básicas.",
+  },
+  {
+    id: 2,
+    nome: "Bloco de Tarefas",
+    descricao: "Aplicação para adicionar, listar e remover tarefas.",
+  },
+];
+
+export default projects;
