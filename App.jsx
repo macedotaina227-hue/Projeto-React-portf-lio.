c722259dad172d03180a44200d941f52341c800b
macedotaina227-hue <macedotaina227@gmail.com>
2026-04-17 19:07:15 -0300

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import About from "./About";
+import Projects from "./Projects";
+
+function App() {
+  return (
+    <div>
+      <About />
+      <Projects />
+    </div>
+  );
+}
+
+export default App;
